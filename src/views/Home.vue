79792dda33d3257bf506bce8de11debcaab87a31
Solst1tium <solange.leiva@mayor.cl>
2020-09-03 22:32:43 -0400

    barra de navegacion, seccion con 3 columnas de elementos con componente home para visualizar personajes del data, utilizando vuetify

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,63 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-row>
+      <v-col  cols="12" sm="10" offset-sm="1">
+        <v-card>
+          <v-container fluid>
+            <v-row>
+              <v-col v-for="personaje in personajes" :key="personaje.id" class="d-flex child-flex" cols="4">
+                <v-card flat tile class="d-flex flex-column">
+                  <v-img :src="personaje.imagen" aspect-ratio="1" class="grey lighten-2"></v-img>  
+                    <v-card-title>{{personaje.nombre}}</v-card-title>                 
+                </v-card>                  
+              </v-col>                    
+            </v-row>
+          </v-container>
+          </v-card>
+      </v-col>
+    </v-row>  
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
-  }
+   
+  },
+  data() {
+    return {
+      personajes: [
+      {
+      'id': 1,
+      'nombre': 'Morty',
+      'imagen': 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+      },
+      {
+      'id': 2,
+      'nombre': 'Rick',
+      'imagen': 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+      },
+      {
+      'id': 3,
+      'nombre': 'Summer',
+      'imagen': 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
+      },
+      {
+      'id': 4,
+      'nombre': 'Beth',
+      'imagen': 'https://rickandmortyapi.com/api/character/avatar/4.jpeg'
+      },
+      {
+      'id': 5,
+      'nombre': 'Jerry',
+      'imagen': 'https://rickandmortyapi.com/api/character/avatar/5.jpeg'
+      }
+      ]
+    }
+  },
 }
 </script>
---
+++ src/views/Login.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <v-card width="400px" class="mx-auto mt-5" >
+      <v-card-title primary-title>
+        <h1 class="mx-auto">Login</h1>
+      </v-card-title>
+      <v-card-text> 
+        <v-form>
+          <v-text-field name="name" label="userName" prepend-icon="mdi-account-circle"></v-text-field>
+          <v-text-field 
+            @click:append= "showPassword = !showPassword" 
+            :type="showPassword ? 'text' : 'password'" 
+            label="showPassword" prepend-icon="mdi-lock"
+            :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'">
+          </v-text-field>
+        </v-form>
+      </v-card-text> 
+      <v-card-actions>
+        <v-btn color="success">Register</v-btn>
+        <v-spacer></v-spacer>
+        <v-btn color="info">login</v-btn>        
+      </v-card-actions>
+    </v-card>    
+  </div>
+</template>
+<script>
+export default {
+   data: () => ({
+    showPassword: false//para q no se vea l a contraseña
+  }),
+}
+</script>
