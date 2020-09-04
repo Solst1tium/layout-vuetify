98f068a7bc7cf513e26b03a0e7b05860881842ce
Solst1tium <solange.leiva@mayor.cl>
2020-09-03 22:46:47 -0400

    se optimizó el codigo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,17 @@
 <template>
   <div class="home">
     <v-row>
-      <v-col  cols="12" sm="10" offset-sm="1">
-        <v-card>
+      <v-col  cols="12" sm="10" offset-sm="1">     
           <v-container fluid>
             <v-row>
               <v-col v-for="personaje in personajes" :key="personaje.id" class="d-flex child-flex" cols="4">
-                <v-card flat tile class="d-flex flex-column">
+                <v-card tile class="d-flex flex-column">
                   <v-img :src="personaje.imagen" aspect-ratio="1" class="grey lighten-2"></v-img>  
                     <v-card-title>{{personaje.nombre}}</v-card-title>                 
                 </v-card>                  
               </v-col>                    
             </v-row>
-          </v-container>
-          </v-card>
+          </v-container>        
       </v-col>
     </v-row>  
   </div>
